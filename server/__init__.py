f92e1197d7dc13af5df24f18d5f8eaa45ea7e516
AshiqAbdulkhader <ashiqabdulkhadder@gmail.com>
2026-04-25 12:30:01 +0530

    create openenv boilerplate

---
+++ server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Kubemedic environment server components."""
+
+from .Kubemedic_environment import KubemedicEnvironment
+
+__all__ = ["KubemedicEnvironment"]
